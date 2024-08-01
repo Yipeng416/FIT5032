ae081bb19dd77a6434b6ee864cb2b8c5e1366446
yipeng <ywan0855@monash.student.monash.edu>
2024-08-01 23:49:01 +0800

    week1

---
+++ src/components/MyName.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Welcome to Vue.js, Yipeng.</div>
+</template>
+
+<script>
+export default {
+  name: 'MyName'
+}
+</script>
+
+<style scoped>
+div {
+  font-size: 20px;
+  color: #333;
+}
+</style>
